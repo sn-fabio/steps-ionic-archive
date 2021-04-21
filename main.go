65bf27df24c8dc16f975c560784b5b52dcbf85d0
sn-fabio <74429437+sn-fabio@users.noreply.github.com>
2021-04-21 15:13:45 -0400

    Update main.go

--- main.go
+++ main.go
@@ -463,7 +463,7 @@ func main() {
 
 	// if android in platforms
 	if len(apks) == 0 && sliceutil.IsStringInSlice("android", platforms) {
-		fail("No apk generated: %d :: %v", len(apks), platforms)
+		fail("No apk generated: %d :: %s :: %v", len(apks), androidOutputDir, platforms)
 	}
 	// if ios in platforms
 	if sliceutil.IsStringInSlice("ios", platforms) {
